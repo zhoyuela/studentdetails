4ee80fdd5b6b8e0d81ee134cb79bec05d97b8abf
zhoyuela <zhoyuela@sheridancollege.ca>
2024-01-30 21:02:39 -0500

    Revert "version 2 student code"
    This reverts commit 0f21d7d464ebb295ee83284cda34a8e9797f8107.

--- src/printstudentdetails/Student.java
+++ src/printstudentdetails/Student.java
@@ -13,7 +13,6 @@ public class Student
 {
    private String name; // Encapsulation principle
    private int age;// getters /setters method
-   private int id;
 
     /**
      * @return the name
@@ -41,19 +40,5 @@ public class Student
      */
     public void setAge(int age) {
         this.age = age;
-    }
-
-    /**
-     * @return the id
-     */
-    public int getId() {
-        return id;
-    }
-
-    /**
-     * @param id the id to set
-     */
-    public void setId(int id) {
-        this.id = id;
     }
 }
